d23f23139255e8e951ff936f094f1615dab3ec2d
Kaung Htet Myint <kaunghtetmyint1999@gmail.com>
2017-11-29 23:15:03 +0630

    Screen Implementation Step1 : search the Images to implement like the Attach Screen. Step2 : layout the ImageView components like the Attach Screen. Step3 : Sign In with E-Mail is not ready for implementation.

---
+++ PADC3FKHMCuriosityAssignment/app/src/main/java/khm/kaunghtetmyint/padc_3_f_khm_curiosityassignment/MainActivity.java
@@ -0,0 +1,13 @@
+package khm.kaunghtetmyint.padc_3_f_khm_curiosityassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
